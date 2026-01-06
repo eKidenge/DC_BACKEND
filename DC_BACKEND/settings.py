23f4dcf01c0e9c4bfa2e7813e2b4bb739585f25c
eKidenge <elishakidenge@gmail.com>
2026-01-06 08:14:51 +0300

    Update settings.py for backend configuration

--- DC_BACKEND/settings.py
+++ DC_BACKEND/settings.py
@@ -157,9 +157,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Custom User Model
 AUTH_USER_MODEL = 'accounts.User'
 
+# REST Framework
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
