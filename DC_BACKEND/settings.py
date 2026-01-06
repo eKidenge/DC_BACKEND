6d4c46892961d3665f95da251d904513fc45c016
eKidenge <elishakidenge@gmail.com>
2026-01-06 07:03:20 +0300

    Save current working state before JWT changes

--- DC_BACKEND/settings.py
+++ DC_BACKEND/settings.py
@@ -157,11 +157,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Custom User Model
 AUTH_USER_MODEL = 'accounts.User'
 
-# REST Framework
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
